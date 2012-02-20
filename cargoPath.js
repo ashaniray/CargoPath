2379133d9ca92fcf6467294f2ec6bffc1902c2d7
Ashani <ashaniray@gmail.com>
2012-02-20 21:00:27 +0530

    First commit

---
+++ cargoPath.js
@@ -0,0 +1,136 @@
+/*
+	cargoPath.js - Displays shortest path from one port to another
+	Depends on:
+
+	1. astar.js
+	2. Graph.js
+
+	Author: Ashani Ray (ashaniray@gmail.com)
+	Date: 04/02/2012
+
+	grid is addressed by grid[x][y]
+	x is latitude and incrases downwards. x=grid.length
+	y is longitude and increased along the array length. y=grid[x].length
+
+*/
+
+var cargoPath = {
+
+	map : null,
+
+	initialize : function () {
+		'use strict';
+
+		var myOptions = {
+			center: new google.maps.LatLng(23, 88),
+			zoom: 2,
+			mapTypeId: google.maps.MapTypeId.ROADMAP
+		};
+		this.map = new google.maps.Map(document.getElementById("map_canvas"),
+				  myOptions);
+	},
+
+	euclidean_distance: function (pos0, pos1) {
+		'use strict';
+
+		var lat1 = mapToGrid.X2lat(pos0.x),
+			lon1 = mapToGrid.Y2lng(pos0.y),
+			lat2 = mapToGrid.X2lat(pos1.x),
+			lon2 = mapToGrid.Y2lng(pos1.y),
+			xdiff = Math.abs(lat1 - lat2),
+			ydiff = Math.abs(lon1 - lon2),
+			dPyth = Math.sqrt(xdiff * xdiff + ydiff * ydiff);
+
+		return dPyth;
+	},
+
+	spherical_distance: function (pos0, pos1) {
+		'use strict';
+
+		var degree2Rad = (Math.PI) / 180.0,
+			lat1 = mapToGrid.X2lat(pos0.x) * degree2Rad,
+			lon1 = mapToGrid.Y2lng(pos0.y) * degree2Rad,
+			lat2 = mapToGrid.X2lat(pos1.x) * degree2Rad,
+			lon2 = mapToGrid.Y2lng(pos1.y) * degree2Rad,
+			//R = 6371, // km
+			R = 1,
+			d = Math.acos(Math.sin(lat1) * Math.sin(lat2) +
+				  Math.cos(lat1) * Math.cos(lat2) *
+				  Math.cos(lon2 - lon1)) * R;
+
+		return d;
+	},
+
+	earth_distance: function (pos0, pos1) {
+		'use strict';
+
+		return cargoPath.spherical_distance(pos0, pos1);
+		//return cargoPath.euclidean_distance(pos0, pos1);
+	},
+
+	isLand: function (latitude, longitude) {
+		'use strict';
+
+		var y = mapToGrid.lng2Y(parseInt(longitude, 10)),
+			x = mapToGrid.lat2X(parseInt(latitude, 10)),
+			grid = mapToGrid.getWorld();
+		return grid[x][y];
+	},
+
+	getPath: function (latSrc, longSrc, latDest, longDest) {
+		'use strict';
+
+		var seaCoordinates = [ new google.maps.LatLng(latSrc, longSrc) ],
+			graph = new Graph(mapToGrid.getWorld()),
+			src_x = mapToGrid.lat2X(latSrc),
+			src_y = mapToGrid.lng2Y(longSrc),
+			dest_x = mapToGrid.lat2X(latDest),
+			dest_y = mapToGrid.lng2Y(longDest),
+			start = graph.nodes[src_x][src_y],
+			end = graph.nodes[dest_x][dest_y],
+			result,
+			i;
+
+		result = astar.search(graph.nodes, start, end, this.earth_distance);
+
+		for (i = 0; i < result.length; i += 1) {
+			seaCoordinates[seaCoordinates.length] = new google.maps.LatLng(
+				mapToGrid.X2lat(result[i].x),
+				mapToGrid.Y2lng(result[i].y)
+			);
+		}
+
+		seaCoordinates[seaCoordinates.length] =  new google.maps.LatLng(latDest, longDest);
+		return seaCoordinates;
+	},
+
+	drawPath: function (latSrc, longSrc, latDest, longDest) {
+		'use strict';
+
+		var seaCoordinates = this.getPath(latSrc, longSrc, latDest, longDest),
+			seaPath;
+
+		if (seaCoordinates.length > 2) {
+			seaPath = new google.maps.Polyline(
+				{
+					path: seaCoordinates,
+					strokeColor: "#FF0000",
+					strokeOpacity: 1.0,
+					strokeWeight: 2
+				}
+			);
+			seaPath.setMap(this.map);
+
+			google.maps.event.addListener(seaPath,
+				'click',
+				function (e) {
+					seaPath.setMap(null);
+					seaPath = null;
+				}
+				);
+		} else {
+			alert('No Path');
+		}
+	}
+};
+
---
+++ cargoPathClient.js
@@ -0,0 +1,109 @@
+/*
+	cargoPathClient.js - Client for cargoPath.js
+	Depends on:
+	1. astar.js
+	2. Graph.js
+	3. cargoPath.js
+
+	Author: Ashani Ray (ashaniray@gmail.com)
+	Date: 04/02/2012
+
+*/
+
+var cargoPathClient = {
+
+	src: null,
+	dest: null,
+
+	updateLink: function (position) {
+		'use strict';
+
+		if (cargoPathClient.src !== null && cargoPathClient.dest !== null) {
+			cargoPathClient.src = cargoPathClient.dest;
+			cargoPathClient.dest = position;
+		} else if (cargoPathClient.src !== null && cargoPathClient.dest === null) {
+			cargoPathClient.dest = position;
+		} else if (cargoPathClient.src === null && cargoPathClient.dest === null) {
+			cargoPathClient.src = position;
+		}
+	},
+
+	placeMarker: function (position, map) {
+		'use strict';
+
+		var lat = parseInt(position.lat(), 10),
+			lng = parseInt(position.lng(), 10),
+			isLand,
+			newPosition,
+			marker;
+
+		isLand = cargoPath.isLand(lat, lng);
+		if (isLand === 0) {
+			newPosition = new google.maps.LatLng(lat, lng);
+			marker = new google.maps.Marker({
+				position: newPosition,
+				map: map,
+				animation: google.maps.Animation.DROP,
+				title: newPosition.toString()
+			});
+			cargoPathClient.updateLink(newPosition);
+
+			google.maps.event.addListener(marker,
+				'click',
+				function (e) {
+					marker.setMap(null);
+					marker = null;
+				});
+		}
+	},
+
+	initialize: function () {
+		'use strict';
+
+		var input, autocomplete,
+			infowindow = new google.maps.InfoWindow();
+
+		cargoPath.initialize();
+		google.maps.event.addListener(
+			cargoPath.map,
+			'click',
+			function (e) {
+				cargoPathClient.placeMarker(e.latLng, cargoPath.map);
+			}
+		);
+		input = document.getElementById('searchTextField');
+		autocomplete = new google.maps.places.Autocomplete(input);
+		autocomplete.bindTo('bounds', cargoPath.map);
+
+		google.maps.event.addListener(
+			autocomplete,
+			'place_changed',
+			function () {
+				var place = autocomplete.getPlace();
+				cargoPath.map.setCenter(place.geometry.location);
+				cargoPath.map.setZoom(2);
+
+				infowindow.setOptions({position: place.geometry.location});
+				infowindow.setContent('<div><strong>' + place.name
+									+ '</strong><br>' + 'Click here to add '
+									+ place.name + ' as a port');
+				infowindow.open(cargoPath.map);
+			}
+		);
+
+	},
+
+	drawPath: function () {
+		'use strict';
+
+		if (this.src !== null && this.dest !== null) {
+			cargoPath.drawPath(parseInt(this.src.lat(), 10),
+				parseInt(this.src.lng(), 10),
+				parseInt(this.dest.lat(), 10),
+				parseInt(this.dest.lng(), 10)
+				);
+
+		}
+	}
+};
+
